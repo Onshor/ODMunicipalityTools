2c17784f8b2bd54601b11ac4694f8210e65d54f0
root <med.dahas@gmail.com>
2018-11-21 17:00:20 +0100

    user_list

--- municipal_app/manage.py
+++ municipal_app/manage.py
@@ -6,12 +6,14 @@ import os
 import unittest
 import coverage
 import csv
+from pandas import pandas as pd
+from pprint import pprint as pp
 
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from project import app, db
-from project.models import User, Municipality, Auto_update, Modules, Users_Models
+from project.models import User, Municipality, Auto_update, Modules, Users_Models, Permisconstruct, Proprietemunicipal, Fourrier, Budget_parametre, File_log
 from list_municipality import municipalitys
 from project.config import DevelopmentConfig as APP_SETTINGS
 
@@ -78,6 +80,37 @@ def create_new():
 def drop_db():
     """Drops the db tables."""
     db.drop_all()
+
+
+@manager.command
+def get_user_data():
+    list_user = []
+    email_list = ['med.dahas@gmail.com', 'kimoensi@gmail.com', 'kamel.mellah@gmail.com', 'mdalloua@fsvc.org', 'habib@onshor.org', 'bochra@metouia.com']
+    list_user_db = [u.__dict__ for u in User.query.all()]
+    for u in list_user_db:
+        if not u['deleted'] and u['activate'] and int(u['municipal_id']) != 1 and u['email'] not in email_list:
+            budget_data = True if Budget_parametre.query.filter_by(user_id=int(u['id'])).first() else False
+            if not budget_data:
+                list_file_budget = [_.id for _ in Auto_update.query.filter_by(municipal_id=u['municipal_id'], category='Budget').all()]
+                for f in list_file_budget:
+                    if File_log.query.filter_by(user_id=int(u['id']), file_id=int(f)).first():
+                        budget_data = True
+                        break
+            list_user.append({'Municipalité': Municipality.query.filter_by(municipal_id=u['municipal_id']).first().municipal_name,
+                              'Nom_Prenom': u['name'] + ' ' + u['last_name'],
+                              'Fonction': u['work_position'],
+                              'Tel': u['phone_number'],
+                              'Date_Inscription': u['registered_on'].strftime("%m/%d/%Y"),
+                              'Budget_Data': 'Oui' if budget_data else 'Non',
+                              'Fourriere_Data': 'Oui' if Fourrier.query.filter_by(user_id=int(u['id'])).first() else 'Non',
+                              'Permis_du_construction_Data': 'Oui' if Permisconstruct.query.filter_by(user_id=int(u['id'])).first() else 'Non',
+                              'Property_municipal_Data': 'Oui' if Proprietemunicipal.query.filter_by(user_id=int(u['id'])).first() else 'Non'})
+    pp(list_user)
+    pp(len(list_user))
+    df = pd.DataFrame(list_user)
+    cols = ["Municipalité", "Nom_Prenom", "Fonction", "Tel", "Date_Inscription", "Budget_Data", "Fourriere_Data", "Permis_du_construction_Data", "Property_municipal_Data"]
+    df.to_csv('list_users.csv', index=False, encoding='utf-8', columns=cols)
+    return 0
 
 
 @manager.command
